2093aeb798194e891a8a2f1e16954fe2304d4561
sukanyaneog <sukanyaneogknssbn@gmail.com>
2025-03-10 23:29:01 +0530

    Container with most water

---
+++ Day13/Container.java
@@ -0,0 +1,22 @@
+
+class Container {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while(left<=right) {
+            int width = right - left;
+            int minheight = Math.min(height[left],height[right]);
+            int area = width * minheight;
+            maxArea = Math.max(maxArea,area);
+
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
