a78dd03a3321320d5f6ebea630eaa3b157ef291c
sukanyaneog <sukanyaneogknssbn@gmail.com>
2025-04-06 11:34:46 +0530

    sorted and rotated

---
+++ Day17/SortRotate.java
@@ -0,0 +1,18 @@
+package Day17;
+
+public class SortRotate {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i]>nums[(i+1)%n]) {
+                count++;
+            }
+            if(count>1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
