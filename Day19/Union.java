f3b458b69e61d33cbd5c6955fe05e6a33af06a13
sukanyaneog <sukanyaneogknssbn@gmail.com>
2025-04-08 21:36:06 +0530

    union of two sorted arrays

--- Day19/Union.java
+++ Day19/Union.java
@@ -7,23 +7,15 @@ public class Union {
     
     static ArrayList<Integer> FindUnion(int arr1[], int arr2[]) {
         int n = arr1.length;
-        int m = arr2.length;
+        int n2 = arr2.length;
         HashSet <Integer> s=new HashSet<>();
         ArrayList < Integer > Union=new ArrayList<>();
         for (int i = 0; i < n; i++)
             s.add(arr1[i]);
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < n2; i++)
             s.add(arr2[i]);
         for (int it: s)
             Union.add(it);
         return Union;
     }
-    public static void main(String args[]) {
-        int arr1[] = {-7,8};
-        int arr2[] = {-9,-8,-3,8};
-        ArrayList<Integer> Union = FindUnion(arr1, arr2);
-        System.out.println("Union of arr1 and arr2 is ");
-        for (int val: Union)
-            System.out.print(val+" ");
-        }
 }
