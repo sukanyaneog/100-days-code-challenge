6e0c91ed6cf30106cd8727eeda0058f7323c4513
sukanyaneog <sukanyaneogknssbn@gmail.com>
2025-04-13 20:12:10 +0530

    Single number

---
+++ Day23/SingleNumber.java
@@ -0,0 +1,12 @@
+package Day23;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+        for (int i=0; i<n; i++) {
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+}
