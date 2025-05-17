6c4f0fa4a57068d97409797c011f59dde9045c1c
sukanyaneog <sukanyaneogknssbn@gmail.com>
2025-05-18 00:50:54 +0530

    Sort Colors

---
+++ Day24/SortColors.java
@@ -0,0 +1,30 @@
+package Day24;
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int count0 = 0;
+        int count1 = 0;
+        for (int i=0; i<n; i++) {
+            if(nums[i]==0) {
+                count0++;
+            }
+            else if(nums[i]==1) {
+                count1++;
+            }
+        }
+
+        for (int i=0; i<count0; i++){
+            nums[i] = 0;
+        }
+        for (int i=count0; i<count0+count1; i++){
+            nums[i] = 1;
+        }
+        for (int i=count0+count1; i<n; i++){
+            nums[i] = 2;
+        }
+        for (int i=0; i<n; i++){
+            System.out.print(nums[i]);
+        }
+    }
+}
